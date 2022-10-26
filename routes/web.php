ac9974d2f58b013242f1911968bd4c4b29b4be35
Ibrahim Ahmad <210029073@aston.ac.uk>
2022-10-26 11:31:48 +0100

    Added routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('contacts', function () {
+    return view('contact');
+});
+
+Route::get('home', function () {
+    return view('home');
+});
+
+Route::get('login', function () {
+    return view('login');
+});
+
+Route::get('product', function () {
+    return view('product');
+});
+
+Route::get('register', function () {
+    return view('register');  
+});
+
+Route::get('products', function () {
+    return view('products');
+});
