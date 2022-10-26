aba4cd09fd4e955286208864458d3ee58c3d3762
Ibrahim Ahmad <210029073@aston.ac.uk>
2022-10-26 15:06:35 +0100

    Prepared the order, orderline, personnel, products table

--- database/migrations/2022_10_26_105713_create_personnel_table.php
+++ database/migrations/2022_10_26_105713_create_personnel_table.php
@@ -8,13 +8,18 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
+     * This will setup each fields within the table, according to each of their data types.
+     * @author Ibrahim Ahmad <210029073@aston.ac.uk>
      * @return void
      */
     public function up()
     {
         Schema::create('personnel', function (Blueprint $table) {
-            $table->id();
+            $table->id('personnel_id');
+            $table->string('personnel_first_name', 31);
+            $table->string('personnel_last_name', 31);
+            $table->string('personnel_username', 255);
+            $table->string('personnel_password', 255);
             $table->timestamps();
         });
     }
--- database/migrations/2022_10_26_105758_create_order_table.php
+++ database/migrations/2022_10_26_105758_create_order_table.php
@@ -8,14 +8,17 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
+     * This will setup each fields within the order table, according to each of their data types.
+     * @author Ibrahim Ahmad <210029073@aston.ac.uk>
      * @return void
      */
     public function up()
     {
         Schema::create('order', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->id('order_ref_no');
+            $table->foreignId('customerID');
+            $table->dateTime('order_date');
+            $table->float('total');
         });
     }
 
