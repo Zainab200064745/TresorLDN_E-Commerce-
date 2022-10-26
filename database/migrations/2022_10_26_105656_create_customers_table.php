da2530ac9efdc5f7b71f16011785f2ee77c8fbb9
Ibrahim Ahmad <210029073@aston.ac.uk>
2022-10-26 14:32:55 +0100

    Added fields to customer table

--- database/migrations/2022_10_26_105656_create_customers_table.php
+++ database/migrations/2022_10_26_105656_create_customers_table.php
@@ -14,7 +14,15 @@ return new class extends Migration
     public function up()
     {
         Schema::create('customers', function (Blueprint $table) {
-            $table->id();
+            $table->id('customer_id');
+            $table->string('customer_first_name', 31);
+            $table->string('customer_last_name', 31);
+            $table->text('customer_address_line_1');
+            $table->text('customer_address_line_2');
+            $table->text('customer_postcode');
+            $table->string('customer_status');
+            $table->string('username', 255);
+            $table->string('password', 255);
             $table->timestamps();
         });
     }
