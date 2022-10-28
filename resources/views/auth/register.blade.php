2b55b1f65a44174431364fa0d8680360e6ef573f
Ibrahim Ahmad <210029073@aston.ac.uk>
2022-10-28 23:06:50 +0100

    Made changes to login page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,13 +12,69 @@
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('First Name') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                <input id="user_first_name" type="text" class="form-control @error('user_first_name') is-invalid @enderror" name="user_first_name" value="{{ old('user_first_name') }}" required autocomplete="user_first_name" autofocus>
 
-                                @error('name')
+                                @error('user_first_name')
                                     <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="user_last_name" class="col-md-4 col-form-label text-md-end">{{ __('Last Name') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="user_last_name" type="text" class="form-control @error('user_last_name') is-invalid @enderror" name="user_last_name" value="{{ old('user_last_name') }}" required autocomplete="user_last_name" autofocus>
+
+                                @error('user_last_name')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="user_address_line_1" class="col-md-4 col-form-label text-md-end">{{ __('Customer Address Line 1') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="user_address_line_1" type="text" class="form-control @error('user_address_line_1') is-invalid @enderror" name="user_address_line_1" value="{{ old('user_address_line_1') }}" required autocomplete="user_address_line_1" autofocus>
+
+                                @error('user_address_line_1')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="user_address_line_2" class="col-md-4 col-form-label text-md-end">{{ __('Customer Address Line 2') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="user_address_line_2" type="text" class="form-control @error('user_address_line_2') is-invalid @enderror" name="user_address_line_2" value="{{ old('user_address_line_2') }}" required autocomplete="user_address_line_2" autofocus>
+
+                                @error('user_address_line_2')
+                                <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="user_postcode" class="col-md-4 col-form-label text-md-end">{{ __('Customer Postcode') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="user_postcode" type="text" class="form-control @error('user_postcode') is-invalid @enderror" name="user_postcode" value="{{ old('user_postcode') }}" required autocomplete="user_postcode" autofocus>
+
+                                @error('user_postcode')
+                                <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
                                 @enderror
